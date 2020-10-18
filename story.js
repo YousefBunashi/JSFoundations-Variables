29b24091ab14d1b8e26ec274744124ccac327046
YousefBunashi <bunashi88@hotmail.com>
2020-10-18 20:18:47 +0300

    my story

---
+++ story.js
@@ -0,0 +1,12 @@
+let name = "yousef";
+
+let age = 32;
+
+let hooby = "football";
+
+console.log (` Mr.${name}, in 33 years, you will be ${age+33}  years old. By then you'll have decades of ${hooby} practice!`);
+
+
+
+
+
